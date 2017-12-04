88306652de9194ce7ce120ea4a3e7db5f8b9980b
ZhangScript <616670175@qq.com>
2017-12-04 13:14:19 -0500

    dzhang: test push, ignore this commit

--- run_lengthcut.m
+++ run_lengthcut.m
@@ -3,4 +3,5 @@ clear all; close all; clc;
 NUM_SPLIT = [1 14]; % row and colum
 %% Algorithm Running
 split_paper_main
+
 vertical_bar_reconstruction_main
--- split_paper_main.m
+++ split_paper_main.m
@@ -7,7 +7,8 @@ list=dir('Input Images\');
 OutputFolderName = 'Images of splitting result'; 
 
 % pre-processing, set the threshold to binarize the img 
-IMG_THRE = 0.7;
+IMG_THRE = 0.75;
+%% fixit: list is unkown 
 for i = 3:1:length(list)
     imagename = list(i).name;
     imagename(end-3:end) = []; % remove the suffix. e.g, .png
@@ -15,4 +16,4 @@ for i = 3:1:length(list)
 end
 
 NAME = 'data_split_images';  %% data_whole_Subs
-save(NAME, 'data')
+save(NAME, 'data');
