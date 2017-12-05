aac43903986e11545ff1fcb28651dd72e265de82
WillenPeng <31708131+WillenPeng@users.noreply.github.com>
2017-12-05 13:56:57 -0500

    refining the code second round

--- split_paper.m
+++ split_paper.m
@@ -10,7 +10,7 @@ function data_split_eachimages = split_paper(image, NUM_SPLIT, OutputFolderName,
     img(img >= IMG_THRE) = 1;
     img(img < IMG_THRE) = 0;
     [row, col] = size(img);
-    mkdir([OutputFolderName, '\', imagename]); % make output folder for splitted images
+    mkdir([OutputFolderName, '/', imagename]); % make output folder for splitted images
     if NUM_SPLIT(1) == 1
         numsplit = NUM_SPLIT(2);
 %         num_random_order = randperm(numsplit);
@@ -21,7 +21,7 @@ function data_split_eachimages = split_paper(image, NUM_SPLIT, OutputFolderName,
             data_split_eachimages.(subname) = img(:, ((num_random_order(j)-1) * step_size + 1) : min(num_random_order(j) * step_size, col));
             figure (j)
             imshow(data_split_eachimages.(subname));
-            figName = [pwd, '\',OutputFolderName, '\', imagename, '\',subname,'.jpg'];
+            figName = [pwd, '/',OutputFolderName, '/', imagename, '/',subname,'.jpg'];
             saveas(figure (j), figName);
             close all;
         end
@@ -38,7 +38,7 @@ function data_split_eachimages = split_paper(image, NUM_SPLIT, OutputFolderName,
                 data_split_eachimages.(subname) = img(((row_split-1) * step_size_row + 1) : min(row_split * step_size_row, row), ((col_split-1) * step_size_col + 1) : min(col_split * step_size_col, col));
                 figure (count)
                 imshow(data_split_eachimages.(subname));
-                figName = [pwd, '\',OutputFolderName, '\', imagename, '\',subname,'.jpg'];
+                figName = [pwd, '/',OutputFolderName, '/', imagename, '/',subname,'.jpg'];
                 saveas(figure (count), figName);
                 close all;
                 count = count + 1;
